c5b2f9bb732273657989d8ad25798efce71d74cb
manskimarcin <39840859+manskimarcin@users.noreply.github.com>
2019-07-18 22:20:56 +0200

    Add files via upload

---
+++ Jenny's sceret message/src/Greeter.java
@@ -0,0 +1,11 @@
+public class Greeter {
+    public static String greet(String name) {
+
+        if(name.equals("Johnny")){
+            return "Hello, my love!";
+        }
+
+        return String.format("Hello, %s!", name);
+
+    }
+}
