6c6b3b651dcca4965a0215f3d7df7db81061d216
manskimarcin <39840859+manskimarcin@users.noreply.github.com>
2019-07-21 16:14:30 +0200

    Add files via upload

---
+++ Invert values/src/Kata.java
@@ -0,0 +1,11 @@
+public class Kata {
+    public static int[] invert(int[] array) {
+        int [] result = new int[array.length];
+
+        for(int i= 0; i < array.length; i++){
+            result[i] = -array[i];
+        }
+
+        return result;
+    }
+}
