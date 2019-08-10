4f2cf7aac8df1d58fcb1890a5859cd4a98b792f7
manskimarcin <39840859+manskimarcin@users.noreply.github.com>
2019-08-10 10:37:07 +0200

    Add files via upload

---
+++ Multiples of 3 or 5/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    public int solution(int number) {
+
+        int result = 0;
+
+        for(int i = 1; i < number ; i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i;
+            }
+
+        }
+        return result;
+    }
+}
