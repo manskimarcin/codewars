238b19e35ccc943dc24de651dd1420110d20e7e7
manskimarcin <39840859+manskimarcin@users.noreply.github.com>
2019-09-22 18:56:28 +0200

    Add files via upload

--- Count of positives sum of negatives/src/Kata.java
+++ Count of positives sum of negatives/src/Kata.java
@@ -2,26 +2,20 @@ public class Kata
 {
     public static int[] countPositivesSumNegatives(int[] input)
     {
-        int positives = 0, negatives = 0;
+        if(input == null || input.length ==0)
+            return new int[]{};
 
-        for(int element: input){
-            System.out.print(element+", ");
-        }
-        System.out.println();
+        int count = 0, negatives = 0;
 
-        for(int i : input){
-            if(i >0){
-                positives = positives +1;
-            //    System.out.println(positives);
-            }
-            else{
-                negatives = negatives  + i;
-             //   System.out.println(negatives);
-            }
+        for(int i = 0; i < input.length; i++){
+            System.out.println(input[i]);
+            if(input[i] > 0)
+                count++;
+            else
+                negatives += Math.abs(input[i]);
         }
-        //int[] result ={positives,negatives};
-        System.out.println(positives+"\t"+negatives);
-        return  new int[]{positives, negatives};
+
+        return  new int[]{count,-negatives};
     }
-
 }
+
